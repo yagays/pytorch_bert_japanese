f3a44f274aaf11775622a1eb1873931bb62a5004
yag_ays <yanagi.ayase@gmail.com>
2019-06-06 15:48:45 +0900

    add use_cuda param

--- bert_juman.py
+++ bert_juman.py
@@ -16,11 +16,12 @@ class JumanTokenizer():
 
 
 class BertWithJumanModel():
-    def __init__(self, bert_path, vocab_file_name="vocab.txt"):
+    def __init__(self, bert_path, vocab_file_name="vocab.txt", use_cuda=False):
         self.juman_tokenizer = JumanTokenizer()
         self.model = BertModel.from_pretrained(bert_path)
         self.bert_tokenizer = BertTokenizer(Path(bert_path) / vocab_file_name,
                                             do_lower_case=False, do_basic_tokenize=False)
+        self.use_cuda = use_cuda
 
     def _preprocess_text(self, text):
         return text.replace(" ", "")  # for Juman
@@ -32,11 +33,15 @@ class BertWithJumanModel():
         ids = self.bert_tokenizer.convert_tokens_to_ids(["[CLS]"] + bert_tokens[:126] + ["[SEP]"]) # max_seq_len-2
         tokens_tensor = torch.tensor(ids).reshape(1, -1)
 
+        if self.use_cuda:
+            tokens_tensor = tokens_tensor.to('cuda')
+            self.model.to('cuda')
+
         self.model.eval()
         with torch.no_grad():
             all_encoder_layers, _ = self.model(tokens_tensor)
 
-        embedding = all_encoder_layers[pooling_layer].numpy()[0]
+        embedding = all_encoder_layers[pooling_layer].cpu().numpy()[0]
         if pooling_strategy == "REDUCE_MEAN":
             return np.mean(embedding, axis=0)
         elif pooling_strategy == "REDUCE_MAX":
